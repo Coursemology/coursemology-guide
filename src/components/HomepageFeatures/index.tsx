3ff8fe864aebd37fd21884c6db4f862351b0d989
jon <cysjonathan@gmail.com>
2023-05-24 04:03:07 +0800

    Update landing page to Coursemology style

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,49 +1,54 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import styles from "./styles.module.css";
 
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  Svg: React.ComponentType<React.ComponentProps<"svg">>;
   description: JSX.Element;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: "Engaging",
+    Svg: require("@site/static/img/icon_engaging.svg").default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Coursemology allows educators to add gamification elements, such as
+        experience points, levels and achievements to their classroom exercises
+        and assignments. The gamification elements motivate students to do their
+        assignments and trainings.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: "General",
+    Svg: require("@site/static/img/icon_general.svg").default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        It's built for all subjects. The gamification system of Coursemology
+        doesn't make any assumptions on the subject. Through Coursemology, any
+        teacher who teaches any subject can turn his course exercises into an
+        online game.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: "Simple",
+    Svg: require("@site/static/img/icon_simple.svg").default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        It's built for all teachers. You don't need to have any programming
+        knowledge to master the platform. Coursemology is easy and intuitive to
+        use for both teachers and students.
       </>
     ),
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({ title, Svg, description }: FeatureItem) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
