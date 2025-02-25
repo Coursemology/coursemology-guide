471d22f42aa13218597de6f03640350bc97a8df8
adi-herwana-nus <adi.hwn@nus.edu.sg>
2025-02-25 11:50:41 +0800

    chore: upgrade package.json dependencies; integrated write-translations script

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,8 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const lightCodeTheme = require("prism-react-renderer").themes.github;
+const darkCodeTheme = require("prism-react-renderer").themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -72,11 +72,6 @@ const config = {
           {
             href: "https://coursemology.org",
             label: "Main Site",
-            position: "right",
-          },
-          {
-            href: "https://github.com/Coursemology/coursemology2",
-            label: "GitHub",
             position: "right",
           },
           {
